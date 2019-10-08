39caf0f27c91efba2ce293460f5ab4fab56333a7
polsala <polsalalidon@gmail.com>
2019-10-08 19:28:39 +0200

    Encrypt to img

--- mozart.py
+++ mozart.py
@@ -1,6 +1,8 @@
 from math import log2, ceil
 from itertools import product
 from functools import reduce
+from PIL import Image
+import numpy as np
 
 ALPHABET_ALL = (
     'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
@@ -12,7 +14,7 @@ ALPHABET_ALL = (
 
 
 class Mozart(object):
-    def __init__(self, alphabet=ALPHABET_ALL, binary_len_key=None, key='sdfskkkllaiiiaiuiifnnal238s', permutation_size=None):
+    def __init__(self, index_start, index_jumps, alphabet=ALPHABET_ALL, binary_len_key=None):
         """
         :param binary_len_key n will be len of binary expression in truth table
         (minimum input number is log2(n) upper round: default)
@@ -21,16 +23,7 @@ class Mozart(object):
         """
 
         self.aphabet_len = len(alphabet)
-        if not permutation_size:
-            self.max_permutations = self.aphabet_len / 2
-        else:
-            permutation_size = int(abs(permutation_size))
-            if permutation_size > self.aphabet_len:
-                raise ValueError('permutation_size should can\'t be greater than alphabet size')
-            else:
-                self.max_permutations = permutation_size
         self.alphabet = alphabet
-        self.key = key
 
         if not binary_len_key:
             self.binary_len_key = ceil(log2(self.aphabet_len))  # 2^n -> upper round n  | 3.1 = 4
@@ -39,29 +32,64 @@ class Mozart(object):
                 raise ValueError('binary_len_key: minimum input number is log2(n) upper round where n is alphabet len')
             else:
                 self.binary_len_key = int(binary_len_key)
-        self.binary_fake_assign = self.create_fake_binary_assign(alphabet, self.binary_len_key)
+        self.binary_fake_assign = self.create_fake_binary_assign(alphabet, self.binary_len_key, index_start, index_jumps)
+        self.rgb_mapping = {}
+        for i, v in self.binary_fake_assign.items():
+            self.rgb_mapping[i] = self.binary_to_rgb(v)
+
+        print(self.binary_fake_assign)
+        print(self.rgb_mapping)
 
     @staticmethod
-    def binary_to_rgb(binary):
-        pass
+    def divide_chunks(l, n):
+        for i in range(0, len(l), n):
+            yield l[i:i + n]
 
     @staticmethod
-    def create_fake_binary_assign(alphabet, permutation_size):
+    def binary_to_rgb(number):
+        B = number % 256
+        G = ((number - B) / 256) % 256
+        R = ((number - B) / 256 ** 2) - G / 256
+
+        return int(R), int(G), int(B)
+
+    @staticmethod
+    def product_index(sorted_list, interesting_pattern):
+        result = 0
+        for number in interesting_pattern:
+            result = result * len(sorted_list) + sorted_list.index(number)
+        return result
+
+    @staticmethod
+    def create_fake_binary_assign(alphabet, permutation_size, index_start, index_jumps):
         # mod rgb colors
         res = {}
-
+        t_size = pow(2, permutation_size)
+        if index_jumps * len(alphabet) + index_start > t_size:
+            raise IndexError('Number of jumps invalid')
         truth_table = list(product([0, 1], repeat=permutation_size))
-        # TODO randomize table with key or implement after in rgb compression
+        indx_list = index_start - 1
         for i, k in enumerate(alphabet):
             # Generate a binary number from true table row
-            res[k] = reduce(lambda a, b: (a << 1) + int(b), truth_table[i])
+            res[k] = reduce(lambda a, b: (a << 1) + int(b), truth_table[indx_list])
+            indx_list += index_jumps
 
         return res
 
-    def encrypt(self, phrase):
-        for i, c in phrase:
-            continue
-        pass
+    def encrypt(self, phrase, split_key):
+        pixels = []
+        for c in phrase:
+            cp = self.rgb_mapping.get(c, None)
+            if cp:
+                pixels.append(cp)
+
+        pixels = list(self.divide_chunks(pixels, split_key))
+        print(pixels)
+
+        array = np.array(pixels, dtype=np.uint8)
+
+        new_image = Image.fromarray(array)
+        new_image.save('new.png')
 
     def decrypt(self):
         pass
